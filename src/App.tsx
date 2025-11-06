d6ae4952281708243cce588b12d2a497d0c3c53d
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-07 04:12:19 +0900

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./css/index.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-3xl text-red-600">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
