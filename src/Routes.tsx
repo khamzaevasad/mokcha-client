bdebeec908f50392fd3a9d8e3c0d883c2e522348
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-10 21:47:39 +0900

    feat: implement routing and add pages for Home, Product, Order, User, and Help

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
+import HomePage from "./pages/home/HomePage";
+import ProductPage from "./pages/productPage/ProductPage";
+import OrderPage from "./pages/orderPage/OrderPage";
+import UserPage from "./pages/userPage/UserPage";
+import HelpPage from "./pages/helpPage/HelpPage";
 
 export function Routes() {
   const routes = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "/products",
+          element: <ProductPage />,
+        },
+        {
+          path: "/orders",
+          element: <OrderPage />,
+        },
+        {
+          path: "/member-page",
+          element: <UserPage />,
+        },
+        {
+          path: "/help",
+          element: <HelpPage />,
+        },
+      ],
     },
   ]);
   return routes;
