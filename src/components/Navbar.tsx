91c5c8c8487a1f3700638476009901b07b77985b
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-10 21:34:09 +0900

    feat: add Navbar component with navigation links and login button

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,80 @@
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const authMember = null;
+  return (
+    <nav className="navbar align-elements my-10">
+      <div className="navbar-start">
+        <NavLink to="/">
+          <h3 className="text-2xl font-bold">Nusret</h3>
+        </NavLink>
+      </div>
+
+      <div className="navbar-end gap-7">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-slate-800 hover:underline underline"
+              : " transition duration-300 ease-in-out text-slate-800"
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-slate-800 hover:underline underline"
+              : " transition duration-300 ease-in-out text-slate-800"
+          }
+          to="/products"
+        >
+          Products
+        </NavLink>
+
+        {authMember ? (
+          <>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-slate-800 hover:underline underline"
+                  : " transition duration-300 ease-in-out text-slate-800"
+              }
+              to="/orders"
+            >
+              Orders
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-slate-800 hover:underline underline"
+                  : " transition duration-300 ease-in-out text-slate-800"
+              }
+              to="/member-page"
+            >
+              My Page
+            </NavLink>
+          </>
+        ) : null}
+
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-slate-800 hover:underline underline"
+              : " transition duration-300 ease-in-out text-slate-800"
+          }
+          to="/help"
+        >
+          Help
+        </NavLink>
+
+        <Button variant="contained" size="small">
+          Login
+        </Button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,24 +1,11 @@
-import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function RootLayout() {
   return (
     <>
       <header>
-        <nav className="navbar">
-          <div className="navbar-start">
-            <NavLink to="/">
-              <h3 className="text-2xl font-bold">Nusret</h3>
-            </NavLink>
-          </div>
-          <div className="navbar-end">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/products">Products</NavLink>
-            <NavLink to="/orders">Orders</NavLink>
-            <NavLink to="/member-page">My Page</NavLink>
-            <NavLink to="/help">Help</NavLink>
-          </div>
-        </nav>
+        <Navbar />
       </header>
       <main>
         <section>
