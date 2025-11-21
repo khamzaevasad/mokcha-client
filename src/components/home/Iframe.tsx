ccf8188f1142508fadf2a921ee2abfd246808d84
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-22 02:19:44 +0900

    feat: update Iframe component to display Seoul location map and add Kakao API key to environment

--- src/components/home/Iframe.tsx
+++ src/components/home/Iframe.tsx
@@ -8,10 +8,11 @@ function Iframe() {
         <div className="w-[120px] h-px border my-3"></div>
       </div>
       <iframe
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Tashkent location map"
         className="w-full h-[400px]"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47991.5361443031!2d69.2040889!3d41.3110815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b05d24c2561%3A0x48b7e61a7d926b53!2sTashkent%2C%20Uzbekistan!5e0!3m2!1sen!2skr!4v1684560123456"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.176197193257!2d126.9868427767348!3d37.5744684720366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2dc50a26c8d%3A0x8250ea36ca52434!2sibis%20Ambassador%20Seoul%20Insadong!5e0!3m2!1sru!2skr!4v1763745285928!5m2!1sru!2skr"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Seoul location map"
       ></iframe>
     </div>
   );
