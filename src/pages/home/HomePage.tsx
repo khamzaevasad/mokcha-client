f2520b715891e2196d713ffee9a92d8d3dfc6189
khamzaevasad <xamzayevasad442@gmail.com>
2025-12-03 19:04:23 +0900

    feat: update HomePage and selector to streamline Redux state retrieval for popular dishes

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { createSelector } from "@reduxjs/toolkit";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
+import { retrieveHomePage, retrievePopularDishes } from "./selector";
 import { Product } from "../../lib/types/product";
 
 /**Redux Slice & Selector**/
@@ -15,81 +15,13 @@ const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
 
-/**
-function actionDispatch(dispatch: Dispatch) {
-return {
-setPopularDishes: function (data: Product[]) {
-return dispatch(setPopularDishes(data));
-},
-};
-} 
- **/
-
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
-
-/**
-const popularDishes = useSelector(function (state: RootState) {
-return retrievePopularDishes(state).popularDishes
-? retrievePopularDishes(state).popularDishes
-: retrievePopularDishes(state);
-}); 
-**/
-
 function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
-  // Selector: Store => Data
+  const { popularDishes } = useSelector(retrieveHomePage);
 
-  useEffect(() => {
-    // Backend server data request => Data
-    const result = [
-      {
-        _id: "690a3ebe00ed3ec5f06abb80",
-        productStatus: "PAUSE",
-        productCollection: "DISH",
-        productName: "Xonim",
-        productPrice: 13,
-        productLeftCount: 100,
-        productSize: "NORMAL",
-        productVolume: 1,
-        productDesc: "Delicious ",
-        productImages: [
-          "uploads/products/5729801a-5000-4f29-bee0-2baefdb04b24.jpg",
-        ],
-        productViews: 0,
-        createdAt: "2025-11-04T17:58:22.556Z",
-        updatedAt: "2025-11-04T17:58:22.556Z",
-        __v: 0,
-      },
-      {
-        _id: "690a3de93df192a92d74e1b6",
-        productStatus: "PAUSE",
-        productCollection: "DRINK",
-        productName: "Juice",
-        productPrice: 4,
-        productLeftCount: 198,
-        productSize: "NORMAL",
-        productVolume: 1,
-        productDesc: "This is good Drink",
-        productImages: [
-          "uploads/products/c015d4a1-7475-40a6-b3e9-da23c953651b.png",
-        ],
-        productViews: 0,
-        createdAt: "2025-11-04T17:54:49.034Z",
-        updatedAt: "2025-11-30T09:46:28.360Z",
-        __v: 0,
-      },
-    ];
+  useEffect(() => {}, []);
 
-    // Slice: Data => Store
-    //@ts-ignore
-    setPopularDishes(result);
-  }, []);
-
-  console.log("popularDishes", popularDishes);
+  console.log("popularDishes from Store", popularDishes);
 
   return (
     <div>
--- src/pages/home/selector.ts
+++ src/pages/home/selector.ts
@@ -5,17 +5,24 @@ const selectHomePage = (state: AppRootState) => {
   return state.homePage;
 };
 
-export const retrievePopularDishes = createSelector(
-  selectHomePage,
-  (HomePage) => {
-    return HomePage.popularDishes;
-  }
-);
-
-export const retrieveNewDishes = createSelector(selectHomePage, (HomePage) => {
-  return HomePage.newDishes;
+export const retrieveHomePage = createSelector(selectHomePage, (homePage) => {
+  return {
+    popularDishes: homePage.popularDishes,
+    newDishes: homePage.newDishes,
+    topUsers: homePage.topUsers,
+  };
 });
 
-export const retrieveTopUsers = createSelector(selectHomePage, (HomePage) => {
-  return HomePage.topUsers;
-});
+// export const retrievePopularDishes = createSelector(
+//   selectHomePage,
+//   (HomePage) => {
+//     return HomePage.popularDishes;
+//   }
+// );
+// export const retrieveNewDishes = createSelector(selectHomePage, (HomePage) => {
+//   return HomePage.newDishes;
+// });
+
+// export const retrieveTopUsers = createSelector(selectHomePage, (HomePage) => {
+//   return HomePage.topUsers;
+// });
