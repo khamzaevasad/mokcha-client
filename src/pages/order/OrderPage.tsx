820cdcc4bd8d12b73659cf5791e0131f06e645e9
khamzaevasad <xamzayevasad442@gmail.com>
2025-12-13 06:15:26 +0900

    feat: implement PausedOrders component; add order management functionality with subtotal and total calculations, and integrate it into OrderPage

--- src/pages/order/OrderPage.tsx
+++ src/pages/order/OrderPage.tsx
@@ -1,5 +1,42 @@
+import PausedOrders from "../../components/order/PausedOrders";
+
 function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <div className="align-elements my-10">
+      <div className="tabs tabs-border">
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab text-foreground font-bold"
+          aria-label="Paused orders"
+          defaultChecked
+        />
+        <div className="tab-content bg-background p-1">
+          <PausedOrders />
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab text-foreground font-bold"
+          aria-label="Process orders"
+        />
+        <div className="tab-content border-black/10 shadow bg-background p-1">
+          Tab content 2
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab text-foreground font-bold"
+          aria-label="Finished orders"
+        />
+        <div className="tab-content border-black/10 shadow bg-background p-1">
+          Tab content 3
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OrderPage;
