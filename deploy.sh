7e4a814a5d7cb5e9cfb491f45d38dcbfcc3183ed
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-26 11:12:37 +0900

    feat: add deploy script for production setup and application start

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#PRODUCTION
+# git reset--hard
+# git checkout main
+# git pull origin master
+
+npm i yarn -g
+yarn
+npm run build
+
+pm2 start "yarn run start:prod" --name=MOKCHA-REACT
