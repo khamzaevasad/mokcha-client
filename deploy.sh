71db02369c197ecddae0a31b8e487540cce5336c
khamzaevasad <xamzayevasad442@gmail.com>
2026-02-27 22:40:45 +0900

    fix: correct git commands in deploy script and update Vite config for server settings

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 
 #PRODUCTION
-git reset--hard
+git reset --hard
 git checkout main
-git pull origin master
+git pull origin main
 
 npm i yarn -g
 yarn
-npm run build
+yarn build
+
 
 pm2 start "yarn run start:prod" --name=MOKCHA-REACT
